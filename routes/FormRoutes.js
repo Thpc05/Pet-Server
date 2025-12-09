4472b39bf824c7f97e38c5fd5eb98838bef6bc7d
Thpc05 <carneiro.theopm@gmail.com>
2025-12-09 19:07:44 -0300

    pdfv1.2

--- routes/FormRoutes.js
+++ routes/FormRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
-import { postForm, getFormById } from '../controllers/FormController.js'; 
+// 1. Adicione o downloadPdf na importação
+import { postForm, getFormById, downloadPdf } from '../controllers/FormController.js'; 
 
 const FormsRoutes = express.Router();
 
 FormsRoutes.post('/postForm', postForm);
+
+// Rota para pegar os DADOS (JSON) para exibir na tela
 FormsRoutes.get('/getForm/:cpf', getFormById);
+
+// 2. Nova Rota para BAIXAR O ARQUIVO (PDF)
+FormsRoutes.get('/downloadPdf/:cpf', downloadPdf);
 
 export default FormsRoutes;
